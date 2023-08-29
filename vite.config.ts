660d685aa703bb78123ec26311c745a9e81f3119
Rewbed <rewbed@gmail.com>
2023-08-29 16:00:06 +0300

    + vite-plugin-singlefile

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import { viteSingleFile } from "vite-plugin-singlefile"
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), viteSingleFile()],
   build: {
-    outDir: './docs',
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.').at(1);
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = 'img';
-          }
-          return `long_island/assets/${extType}/[name]-[hash][extname]`;
-        },
-        chunkFileNames: 'long_island/assets/[name]-[hash].js',
-        entryFileNames: 'long_island/assets/[name]-[hash].js',
-},
-},
+    outDir: './docs'
   }
 })
