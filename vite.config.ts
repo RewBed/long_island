35df63ebf528a85813539e070f79f12d171d208a
Rewbed <rewbed@gmail.com>
2023-08-29 15:45:08 +0300

    + vite config rollupOptions

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,19 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: './docs'
+    outDir: './docs',
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `long_island/assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'long_island/assets/[name]-[hash].js',
+        entryFileNames: 'long_island/assets/[name]-[hash].js',
+},
+},
   }
 })
